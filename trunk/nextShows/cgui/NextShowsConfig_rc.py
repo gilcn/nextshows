78ce835a20e2d043d012d49b877f17d11f528e09
gcnweb <gcnweb@e6d63c4d-7831-0410-afa5-55a26f18ab34>
2007-05-27 20:44:03 +0000

    Added URL to the Google Code's project URL in the config GUI

--- trunk/nextShows/cgui/NextShowsConfig_rc.py
+++ trunk/nextShows/cgui/NextShowsConfig_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Sat May 26 00:48:23 2007
+# Created: Sun May 27 22:40:55 2007
 #      by: The Resource Compiler for PyQt (Qt v4.2.3)
 #
 # WARNING! All changes made in this file will be lost!
