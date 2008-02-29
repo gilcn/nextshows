f2cf864bf4fd96a0091a515c9ee48264b2c6647b
gcnweb <gcnweb@e6d63c4d-7831-0410-afa5-55a26f18ab34>
2008-02-29 17:13:15 +0000

    Dammit, I am really bored of those markup changes :(...

--- nextShows/libs/TvRage.py
+++ nextShows/libs/TvRage.py
@@ -171,7 +171,7 @@ class TvRage(Http):
         # ...and feed BeautifulSoup
         page = BeautifulSoup(content)
 
-        showName = u''+page('table')[3]('td')[13]('h3')[0].contents[0]
+        showName = u''+page('h3')[0].contents[0]
 
         tools.msgDebug("Parsing content...", __name__)
         # Read each line and extract infos
