002c45a4e3abcd283aa7f131cc6ac02c4272fb56
gcnweb <gcnweb@e6d63c4d-7831-0410-afa5-55a26f18ab34>
2008-03-19 14:42:49 +0000

    Use a QListWidget derived widget in order to support DnD

---
+++ nextShows/cgui/ListWidget.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#######################################################################
+# nextShows - Drag n' Drop aware QListWidget
+# Copyright (C) 2008 Gilles CHAUVIN <gcnweb@gmail.com>
+# $Id$
+#######################################################################
+# Coding: UTF-8, 4 spaces indent, LF line terminator
+#######################################################################
+# This file is part of nextShows.
+#
+# nextShows is free software; you can redistribute  it  and/or  modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either  version 2  of  the License, or
+# (at your option) any later version.
+#
+# nextShows is distributed in the hope that  it  will  be  useful, but
+# WITHOUT  ANY  WARRANTY; without  even   the   implied   warranty  of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE.  See  the  GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU  General  Public  License
+# along  with  nextShows; if  not,  write   to   the   Free   Software
+# Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#######################################################################
+
+from PyQt4.Qt import *
+
+
+class ListWidget(QListWidget):
+    def __init__(self, parent=None):
+        QListWidget.__init__(self, parent)
