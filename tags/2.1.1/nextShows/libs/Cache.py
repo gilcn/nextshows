824e7a9fa713f56c5b58cb4935c67179455450f4
gcnweb <gcnweb@e6d63c4d-7831-0410-afa5-55a26f18ab34>
2008-03-25 20:49:22 +0000

    Tagging 2.1.1

---
+++ tags/2.1.1/nextShows/libs/Cache.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#######################################################################
+# nextShows - Cache management
+# Copyright (C) 2006-2007 Gilles CHAUVIN <gcnweb@gmail.com>
+# $Id$
+#######################################################################
+# Coding: UTF-8, 4 spaces indent, LF line terminator
+#######################################################################
+# This file is part of nextShows.
+#
+# nextShows is free software; you can redistribute  it  and/or  modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either  version 2  of  the License, or
+# (at your option) any later version.
+#
+# nextShows is distributed in the hope that  it  will  be  useful, but
+# WITHOUT  ANY  WARRANTY; without  even   the   implied   warranty  of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE.  See  the  GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU  General  Public  License
+# along  with  nextShows; if  not,  write   to   the   Free   Software
+# Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#######################################################################
+
+
+from libs.Globals import Globals
+from libs.TvRage  import TvRage
+import libs.tools as tools
+
+from datetime import datetime
+import cPickle, os, re
+
+
+class Cache():
+    def __init__(self):
+        # Constants
+        self.CACHEFILEOK       = 0
+        self.CACHEFILEOUTDATED = 1
+        self.CACHEFILENOTFOUND = 2
+
+        # Expiration time (days converted to seconds)
+        self.expiration = 604800    # Default to 1 week
+        # Shows IDs
+        self.showIds    = []
+
+
+    ###############################################################################
+    ## Set expiration
+    ###############################################################################
+    def setExpiration(self, expiration):
+        self.expiration = expiration*24*60*60
+
+
+    ###############################################################################
+    ## Method     : cacheEpisodeList(id)
+    ## Description: Call the Parser and store results to cache
+    ##
+    ## Input args:
+    ##   - id     : Show ID
+    ##
+    ## Output:
+    ##   - True   : Caching OK
+    ##   - False  : Ooopss
+    ###############################################################################
+    def cacheEpisodeList(self, id):
+        # Cache filename
+        cacheFileName = Globals().nsCacheFilePrefix + str( id )
+
+        # Get the data
+        parser = TvRage()
+        epList = parser.getEpisodeList( id )
+
+        if not epList:
+            return False    # In case something went wrong during parsing...
+
+        showEpList = {}
+        showEpList['fetch_time']   = int( datetime.utcnow().strftime("%s") ) # Timestamp (UTC)
+        showEpList['episode_list'] = epList
+
+        # Open the file for writing
+        tools.msgDebug("Writing cache file %s..." % cacheFileName, __name__)
+        fp = open( cacheFileName, "wb" )
+        cPickle.dump( showEpList, fp, cPickle.HIGHEST_PROTOCOL )
+        fp.close()
+
+        # At this point we suppose everything hopefully went well
+        return True
+
+
+    ###############################################################################
+    ## Method     : getCachedEpisodeList(id)
+    ## Description: return episode data from cache
+    ##
+    ## Input args:
+    ##   - id     : Show ID
+    ##
+    ## Output:
+    ##   - List containing episode list
+    ###############################################################################
+    def getCachedEpisodeList(self, id):
+        # Cache filename
+        cacheFileName = Globals().nsCacheFilePrefix + str( id )
+        
+        # Get data
+        tools.msgDebug("Getting data from cache %s..." % cacheFileName, __name__)
+        fp = open( cacheFileName, "rb" )
+        data = cPickle.load( fp )
+        fp.close()
+
+        # Return episode list
+        return data['episode_list']
+
+
+    ###############################################################################
+    ## Method     : checkCacheFile(id)
+    ## Description: Check whether the cache for the given show is valid or not
+    ##
+    ## Input args:
+    ##   - id         : Show ID
+    ##
+    ## Output:
+    ##   - Cache file state (OK, OUTDATED or NOTFOUND)
+    ###############################################################################
+    def checkCacheFile(self, id):
+        # Cache filename
+        cacheFileName = Globals().nsCacheFilePrefix + str( id )
+
+        # Check if a file exists for the given id
+        if os.path.isfile( cacheFileName ):
+            # Check if data are still valid
+            currentTS = int( datetime.utcnow().strftime("%s") )
+            # Show filename
+            cacheFileName = Globals().nsCacheFilePrefix + str( id )
+            # Get data fetch time
+            fp = open( cacheFileName, "rb" )
+            cacheData = cPickle.load( fp )
+            fp.close()
+            cacheFileTS = cacheData['fetch_time']
+
+            if ( cacheFileTS + self.expiration ) < currentTS:
+                return self.CACHEFILEOUTDATED
+        else:
+            return self.CACHEFILENOTFOUND
+
+        return self.CACHEFILEOK
+
+
+    ###############################################################################
+    ## Method     : getStaledCacheFiles()
+    ## Description: Return the list of shows which associated cache file is not found
+    ##              or outdated
+    ##
+    ## Input args:
+    ##   - None
+    ##
+    ## Output:
+    ##   - List containing outdated/staled shows
+    ###############################################################################
+    def getStaledCacheFiles(self):
+        fileList = []
+
+        for id in self.showIds:
+            check = self.checkCacheFile( id )
+            if check != self.CACHEFILEOK:
+                fileList.append( id )
+
+        return fileList
+
+
+    ###############################################################################
+    ## Method     : deleteOldCacheFiles()
+    ## Description: Delete cache files belonging to shows not tracked anymore
+    ##
+    ## Input args:
+    ##   - None
+    ##
+    ## Output:
+    ##   - False  : Something odd happened
+    ##   - True   : Success
+    ###############################################################################
+    def deleteOldCacheFiles(self):
+        tools.msgDebug("Purging cache dir...", __name__)
+        cacheDirContents = os.listdir( Globals().nsCacheDir )
+        cacheFilePrefix = os.path.basename( Globals().nsCacheFilePrefix )
+
+        for id in self.showIds:
+            fileName = cacheFilePrefix + str( id )
+            if fileName in cacheDirContents:
+                index = cacheDirContents.index( fileName )
+                cacheDirContents.pop( index )
+
+        # Remove unwanted files
+        exitFlag = True
+        for file in cacheDirContents:
+            fileName = os.path.join( Globals().nsCacheDir, file )
+            try:
+                os.remove( fileName )
+                tools.msgDebug("Deleted %s..." % fileName, __name__)
+            except:
+                exitFlag = False
+                tools.msgDebug("Error raised while trying to delete %s..." % fileName, __name__)
+
+        return exitFlag
+
+
+    ###############################################################################
+    ## Method     : getNextRefreshTS()
+    ## Description: Scans the cache files searching for the next needed refresh
+    ##              !!! /!\ Takes also into account staled cache files /!\ !!!
+    ##
+    ## Input args:
+    ##   - None
+    ##
+    ## Output:
+    ##   - Next refresh timestamp
+    ###############################################################################
+    def getNextRefreshTS(self):
+        cacheFilePrefix = os.path.basename( Globals().nsCacheFilePrefix )
+        cacheFileNameRE = re.compile( r'^' + cacheFilePrefix + '\d+$' )
+
+        fetchTimeList = []
+        
+        for file in os.listdir( Globals().nsCacheDir ):
+            if cacheFileNameRE.match( file ):
+                cacheFileName = os.path.join( Globals().nsCacheDir, file )
+                # Get data fetch time
+                fp = open( cacheFileName, "rb" )
+                cacheData = cPickle.load( fp )
+                fp.close()
+                fetchTimeList.append( cacheData['fetch_time'] )
+
+        # Keep smaller TS
+        nextTS = -1
+        for ts in fetchTimeList:
+            if nextTS == -1:
+                nextTS = ts
+            else:
+                if ts < nextTS:
+                    nextTS = ts
+
+        if nextTS == -1:
+            return -1
+        else:
+            return ( nextTS + self.expiration )
---
+++ tags/2.1.1/nextShows/nextShows.py
@@ -0,0 +1,329 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#######################################################################
+# nextShows
+# Copyright (C) 2006-2007 Gilles CHAUVIN <gcnweb@gmail.com>
+# $Id$
+#######################################################################
+# Coding: UTF-8, 4 spaces indent, LF line terminator
+#######################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2  of  the  License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be  useful, but
+# WITHOUT  ANY  WARRANTY;  without  even  the   implied   warranty  of
+# MERCHANTABILITY or FITNESS FOR A  PARTICULAR  PURPOSE. See  the  GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU  General  Public  License
+# along with  this  program;  if  not,  write  to  the  Free  Software
+# Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#######################################################################
+
+from   libs.Applet  import Applet
+from   libs.Cache   import Cache
+from   libs.Config  import Config
+from   libs.Data    import Data
+from   libs.Globals import Globals
+import libs.tools   as     tools
+
+import os, sys, thread
+from datetime import date, datetime
+
+import karamba
+
+import warnings
+warnings.simplefilter('ignore', RuntimeWarning) # Needed to wipe out os.tempnam()'s warning messages
+
+
+################################################################################
+## Some global variables
+################################################################################
+g_configGuiPid     = None
+#
+g_dateCheck        = date.today()
+g_nextCacheRefresh = 0
+g_showList         = []         # Tracked shows list
+g_showIds          = []         # Tracked shows IDs
+g_cacheExpiration  = 7          # Cache expiration time (days)
+g_pastDays         = 0
+g_linesMin         = 0
+g_linesMax         = 0
+applet             = Applet()   # Make this available globally
+cache              = Cache()    # Make this available globally
+data               = Data()     # Make this available globally
+################################################################################
+
+
+##[FIXME]#######################################################################
+## Copy the widget files to the nextShows config directory !                  ##
+## SuperKaramba (as provided by KDE 3.5.x) does crash when importing PyQt4:   ##
+## -> just do "from PyQt4 import QtCore, QtGui" and it's dead                 ##
+##                                                                            ##
+## (lame) Workaround:                                                         ##
+## - Extracting files from the SKZ and copying them to nextShows's config dir ##
+##[/FIXME]######################################################################
+def createConfigDirs():
+    # Create dirs
+    mode = Globals().nsDirMode
+    dirs = [ Globals().superKarambaDir,
+             Globals().nsConfDir,
+             Globals().nsCacheDir,
+             Globals().nsCGuiBaseDir ]
+    for dir in Globals().nsCGuiDirs:
+        dirs.append( os.path.join( Globals().nsCGuiBaseDir, dir ) )
+    for dir in dirs:
+        try:
+            os.mkdir( dir, mode )
+            tools.msgDebug("Created dir: %s" % dir, __name__)
+        except OSError:
+            pass
+
+
+###############################################################################
+## Copy the theme files to the user's home directory
+###############################################################################
+def copyThemeFilesToConfigDir(widget):
+    # Copy files
+    for file in Globals().nsCGuiFiles:
+        source = karamba.readThemeFile( widget, file )
+        dest   = open( os.path.join( Globals().nsCGuiBaseDir, file ), "w" )
+        tools.msgDebug("Copying file: %s" % file, __name__)
+        print >> dest, source
+        dest.close()
+
+
+
+###############################################################################
+## Here begins the "pure" SuperKaramba Part                                  ##
+###############################################################################
+#this is called when your widget is initialized
+def initWidget(widget):
+    global g_nextCacheRefresh, g_showList, g_showIds, g_cacheExpiration, g_pastDays, g_linesMin, g_linesMax
+
+    # Pass the widget reference
+    Applet.widget = widget
+
+    # Init splash
+    splash = Applet().Splash()
+    splash.show()
+
+    # Create dir structure
+    splash.setText("Checking config dirs...")
+    createConfigDirs()
+
+    # Read config
+    splash.setText("Reading config...")
+    config = Config()
+
+    # Check whether we want DEBUG messages enabled or not
+    if config.getboolean("main", "debug") == False:
+        tools.msgDebug("Disabling debug messages !", __name__)
+        Globals.DEBUG = False
+
+    # Copy GUI files (if necessary)
+    if Globals().versions['nextShows'] != config.get("main", "version") \
+    or not "launchGUI" in os.listdir( Globals().nsCGuiBaseDir ):
+        # Init dir structures and copy files
+        splash.setText("Setup GUI...")
+        copyThemeFilesToConfigDir(widget)
+        config.set("main", "version", Globals().versions['nextShows'])
+
+    # Get other useful infos from config
+    splash.setText("Reading config...")
+    displayType = config.get("display", "type")
+    if displayType == "Fixed":
+        g_linesMax = config.getint("display", "lines_fixed")
+        g_linesMin = g_linesMax
+    else:
+        g_linesMin = config.getint("display", "lines_min")
+        g_linesMax = config.getint("display", "lines_max")
+    g_cacheExpiration          = config.getint("misc",    "cache_expiration")
+    g_pastDays                 = config.getint("display", "past_days")
+    applet.colorList           = config.getColors()
+    applet.episodeFormatString = config.get(   "display", "format")
+    applet.browser             = config.get(   "misc",    "browser")
+    applet.dateFormat          = config.get(   "display", "date_format")
+    applet.whenFormat          = config.getint("misc",    "when_format")
+
+    # Getting the show list
+    splash.setText("Getting show list....")
+    g_showList = config.getShows()
+
+    # Extract IDs
+    g_showIds = [ show['id'] for show in g_showList ]
+
+    # Init cache
+    cache.setExpiration( g_cacheExpiration )
+    cache.showIds = g_showIds
+
+    # Refresh cache if necessary
+    staledList = cache.getStaledCacheFiles()
+    for id in staledList:
+        for show in g_showList:
+            if show['id'] == id:
+                showName = show['name']
+        splash.setText("Updating cache: '%s'..." % showName)
+        cache.cacheEpisodeList( id )
+
+    # Fetch data to display
+    splash.setText("Filtering episodes...")
+    data = Data()
+    episodeList  = data.getEpisodeList( g_showIds, g_pastDays, g_linesMax )
+    applet.episodeList = episodeList
+
+    # Close the splash
+    splash.setText("Done!")
+    splash.hide()
+
+    # Init widget
+    # Fallback (for compatibility reasons)
+    # "[display] theme=" was moved to "[misc] theme="
+    try:
+        applet.themeName = config.get("misc", "theme")
+    except:
+        applet.themeName = Globals().defaultThemeName
+    numReturnedEpisode = len( episodeList )
+    if numReturnedEpisode < g_linesMin:
+        themeLines = g_linesMin
+    elif numReturnedEpisode > g_linesMax:
+        themeLines = g_linesMax
+    else:
+        themeLines = numReturnedEpisode
+    applet.themeLines = themeLines
+    applet.drawBackground()
+    applet.printEpisodeList()
+
+    # Store next cache refresh
+    g_nextCacheRefresh = cache.getNextRefreshTS()
+
+    # Setup menu entry for config GUI
+    karamba.addMenuConfigOption(widget, "config_gui", "Configure...")
+    karamba.setMenuConfigOption(widget, "config_gui", 0)
+
+
+
+#this is called everytime your widget is updated
+#the update inverval is specified in the .theme file
+def widgetUpdated(widget):
+    global g_configGuiPid, g_dateCheck, g_nextCacheRefresh, g_showList, g_showIds, g_pastDays, g_linesMin, g_linesMax
+
+    tools.msgDebug("Widget update triggered...", __name__)
+
+    # Block widget updates when GUI is running...
+    if g_configGuiPid:
+        tools.msgDebug("Widget updates suspended, GUI's running...", __name__)
+        return
+
+    # If date changed since the widget was launched, refresh view
+    if date.today() != g_dateCheck:
+        tools.msgDebug("Day changed, refreshing view...", __name__)
+        episodeList  = data.getEpisodeList( g_showIds, g_pastDays, g_linesMax )
+        applet.episodeList = episodeList
+        numReturnedEpisode = len( episodeList )
+        if numReturnedEpisode < g_linesMin:
+            themeLines = g_linesMin
+        elif numReturnedEpisode > g_linesMax:
+            themeLines = g_linesMax
+        else:
+            themeLines = numReturnedEpisode
+        applet.themeLines = themeLines
+        applet.drawBackground()
+        applet.printEpisodeList()
+
+        # Reset date check
+        g_dateCheck = date.today()
+
+
+    # Check chether cache needs refresh or no
+    ncrTS = g_nextCacheRefresh
+    nowTS = int( datetime.utcnow().strftime("%s") )
+    if ncrTS == -1:
+        # Cache is empty...
+        return
+    if ncrTS < nowTS:
+        tools.msgDebug("Cache needs refresh...", __name__)
+        # If nextCacheRefresh is past, we need to refresh cache
+        # First, cleanup cache
+        cache.deleteOldCacheFiles()
+        # Refresh cache
+        staledList = cache.getStaledCacheFiles()
+        for id in staledList:
+            for show in g_showList:
+                if show['id'] == id:
+                    showName = show['name']
+            tools.msgDebug("Refreshing cache: '%s'...", __name__)
+            cache.cacheEpisodeList( id )
+
+        # Reset nextCacheRefresh value
+        g_nextCacheRefresh = cache.getNextRefreshTS()
+
+
+
+#This gets called when an item is clicked in the theme CONFIGURATION menu,
+#not the popup menus that you create.
+#  key = the reference to the configuration key that was changed
+#  value = the new value (true or false) that was selected
+def menuOptionChanged(widget, key, value):
+    global g_configGuiPid
+
+    karamba.setMenuConfigOption(widget, "config_gui", 0)
+
+    if key == "config_gui":
+        if g_configGuiPid == None:
+            # Launch config GUI
+            gui=os.path.join( Globals().nsCGuiBaseDir, "launchGUI" )
+            cmd = ["python", gui]
+            g_configGuiPid=karamba.executeInteractive(widget, cmd)
+        else:
+            tools.msgDebug("GUI already running", __name__)
+
+
+
+#This gets called when a meter (image, text, etc) is clicked.
+# NOTE you must use attachClickArea() to make a meter
+# clickable.  
+#  widget = reference to your theme
+#  meter = the meter clicked
+#  button = the button clicked (see widgetClicked for button numbers)
+def meterClicked(widget, meter, button):
+    def storeNewWhenFormat(dummy):
+        config = Config()
+        config.set( "misc", "when_format", str( applet.whenFormat ) )
+        config.close()
+
+    if meter in applet.refEpisodeListWhen:
+        applet.whenFormat = ( applet.whenFormat + 1 ) % 3
+        applet.refreshWhenColumn()
+        thread.start_new_thread( storeNewWhenFormat, ("",) )
+
+
+
+#This gets called when a command you have executed with executeInteractive() outputs something
+#to stdout.  This way you can get the output of for example kdialog without freezing up the widget
+#waiting for kdialog to end.
+#  widget = reference to your theme
+#  pid = process number of the program outputting (use this if you execute more than out process)
+#  output = the text the program outputted to stdout
+def commandOutput(widget, pid, output):
+    global g_configGuiPid
+
+    if pid == g_configGuiPid:
+        config = Config()
+        # Check whether config was changed or not
+        try:
+            configChanged = config.getboolean("main", "config_changed")
+            config.remove_option("main", "config_changed")
+        except:
+            configChanged = False
+
+        config.close()
+
+        if configChanged:
+            karamba.reloadTheme(widget)
+            return
+        else:
+            g_configGuiPid = None
