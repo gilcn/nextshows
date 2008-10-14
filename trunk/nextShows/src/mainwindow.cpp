3f9954adf614b1814c66683a1d0fb96c9a1f802e
gcnweb <gcnweb@e6d63c4d-7831-0410-afa5-55a26f18ab34>
2008-10-14 23:06:54 +0000

    Play a bit with QTableView

--- trunk/nextShows/src/mainwindow.cpp
+++ trunk/nextShows/src/mainwindow.cpp
@@ -21,6 +21,11 @@
 // Own
 #include "mainwindow.h"
 
+// Qt
+#include <QtCore/QDate>
+#include <QtCore/QDebug>
+#include <QtGui/QHeaderView>
+
 
 /*
 ** public:
@@ -28,6 +33,34 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    setupUi(this);
+
+    m_dataModel = new QStandardItemModel(this);
+    m_dataModel->setColumnCount(5);
+    QStringList labels;
+    labels << tr("Show") << tr("Episode name")
+           << tr("Season") << tr("Episode #") << tr("Date");
+    m_dataModel->setHorizontalHeaderLabels(labels);
+    tableView->setModel(m_dataModel);
+    tableView->verticalHeader()->hide();
+    tableView->horizontalHeader()->setResizeMode(QHeaderView::Interactive);
+    tableView->horizontalHeader()->setStretchLastSection(true);
+//    tableView->horizontalHeader()->setCascadingSectionResizes(true);
+    tableView->horizontalHeader()->setHighlightSections(true);
+    tableView->horizontalHeader()->setMovable(true);
+
+    // Fill with random stuff
+    for (int i=0; i<30; ++i) {
+        m_dataModel->setItem(i, 0, new QStandardItem(QString("show_%1").arg(i)));
+        m_dataModel->setItem(i, 1, new QStandardItem(QString("episode_%1").arg(i)));
+        m_dataModel->setItem(i, 2, new QStandardItem(QString("%1").arg(rand()%10+1)));
+        m_dataModel->setItem(i, 3, new QStandardItem(QString("%1").arg(rand()%24+1)));
+        QDate rndDate = QDate::currentDate().addDays(i);
+        m_dataModel->setItem(i, 4, new QStandardItem(rndDate.toString("yyyy/MM/dd")));
+    }
+
+    setWindowTitle("nextShows");
+    statusBar()->showMessage(tr("nextShows started"), 1000*3);
 } // ctor()
 
 MainWindow::~MainWindow()
