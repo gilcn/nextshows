fce8e88b371998d8ed5cacd964cfe858aad05387
gcnweb <gcnweb@e6d63c4d-7831-0410-afa5-55a26f18ab34>
2008-10-23 17:52:28 +0000

    Update Copyright

--- trunk/nextShows/src/libs/getdatatvrage.cpp
+++ trunk/nextShows/src/libs/getdatatvrage.cpp
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2008 Emmanuel Hamelet <kh.starlifter@gmail.com>
+ *   Copyright (C) 2008 Emmanuel Hamelet <kh.starlifter@gmail.com>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as
