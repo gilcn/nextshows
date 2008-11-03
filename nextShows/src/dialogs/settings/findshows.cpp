176568070c422c031cc929abe7c55d90145ca380
gcnweb <gcnweb@e6d63c4d-7831-0410-afa5-55a26f18ab34>
2008-11-03 19:49:53 +0000

    - Lots of internal changes - More work on the "findShows" UI

--- nextShows/src/dialogs/settings/findshows.cpp
+++ nextShows/src/dialogs/settings/findshows.cpp
@@ -43,22 +43,25 @@ FindShows::FindShows(QWidget *parent)
 
 
     ui.leSearch->setClickMessage(tr("Enter a show name"));
+    // FIXME: Qt4.5
 //    ui.leSearch->setIcon(QIcon(":/pixmaps/prefs/progress_idle.png"));
 
 
-    m_animatedImage = new AnimatedImage(this, ":/pixmaps/prefs/progress_working.png");
+    m_animatedImage = new AnimatedImage(this,
+                                        ":/pixmaps/prefs/progress_working.png",
+                                        ":/pixmaps/prefs/progress_idle.png");
     connect(m_animatedImage, SIGNAL(newFrame(const QPixmap &)),
             this, SLOT(newImageFrame(const QPixmap &)));
 
-//    m_data = new GetData(this);
     m_tvrage = new TvRageProvider(this);
-    connect(m_tvrage, SIGNAL(searchShowReady(QVariantList)),
-            this, SLOT(displaySearchResults(const QVariantList &)));
+    connect(m_tvrage, SIGNAL(searchResultsReady(QList<AbstractProvider::SearchResults_t>)),
+            this, SLOT(searchResultsReady(const QList<AbstractProvider::SearchResults_t> &)));
+
+    m_filterResults = (ui.cbFilterResults->checkState() == Qt::Checked) ? true : false;
 } // ctor()
 
 FindShows::~FindShows()
 {
-//    delete m_data;
     delete m_tvrage;
     delete m_animatedImage;
 } // dtor()
@@ -73,44 +76,144 @@ void FindShows::on_btnLookup_clicked()
         return;
     }
 
+    ui.leSearch->setEnabled(false);
     ui.btnLookup->setEnabled(false);
+
     m_animatedImage->start();
     m_tvrage->searchShow(ui.leSearch->text());
 } // on_btnLookup_clicked()
 
+void FindShows::on_cbFilterResults_stateChanged(int state)
+{
+    m_filterResults = (state == Qt::Checked) ? true : false;
+    displaySearchResults(); // Refresh list
+} // on_cbFilterResults_stateChanged()
+
 void FindShows::newImageFrame(const QPixmap &pixmap)
 {
-    ui.btnLookup->setIcon(pixmap);
+//    ui.btnLookup->setIcon(pixmap);
+    ui.imgProgress->setPixmap(pixmap);
 } // newImageFrame()
 
-void FindShows::displaySearchResults(const QVariantList &shows)
+void FindShows::searchResultsReady(const QList<AbstractProvider::SearchResults_t> &showList)
 {
+    m_searchResults = showList;
+
     m_animatedImage->stop();
+
+    ui.leSearch->setEnabled(true);
     ui.btnLookup->setEnabled(true);
 
-    ui.listWidget->clear();
-    foreach(QVariant show, shows) {
-        QVariantMap map(show.toMap());
-        QListWidgetItem *item = new QListWidgetItem();
-        QString flagFile = QString(":/pixmaps/flags/%1.gif").arg(map["country"].toString().toLower());
+    displaySearchResults(); // Refresh list
+} // searchResultsReady()
+
+
+/*
+** private:
+*/
+void FindShows::displaySearchResults()
+{
+    ui.treeSearchResults->clear();
+
+    int displayedShowCounter = 0;
+
+    foreach(AbstractProvider::SearchResults_t show, m_searchResults) {
+        if (m_filterResults && show.ended != 0) {
+            continue; // Skip to the next result when user wants to filter
+        }
+
+        QTreeWidgetItem *parentItem = new QTreeWidgetItem();
+        QFont font(parentItem->font(0));
+        QBrush brush(parentItem->foreground(0));
+        QString flagFile = QString(":/pixmaps/flags/%1.gif").arg(show.country.toLower());
+
         if (QFile::exists(flagFile)) {
-            item->setIcon(QIcon(flagFile));
+            parentItem->setIcon(0, QIcon(flagFile));
         } else {
-            item->setIcon(QIcon(":/pixmaps/flags/unknown.gif"));
+            parentItem->setIcon(0, QIcon(":/pixmaps/flags/unknown.gif"));
         }
-        item->setText(map["name"].toString());
-        if (map["ended"].toInt() == 0) {
-            QFont font(item->font());
-            font.setBold(true);
-            item->setFont(font);
-        }
-        else {
-            QBrush brush(item->foreground());
+
+        parentItem->setText(0, show.name);
+
+        if (show.ended == 0) {
+            if (!m_filterResults) {
+                font.setBold(true);
+                parentItem->setFont(0, font);
+            }
+        } else {
             brush.setColor(Qt::gray);
-            item->setForeground(brush);
+            parentItem->setForeground(0, brush);
         }
-        ui.listWidget->addItem(item);
+
+        ui.treeSearchResults->addTopLevelItem(parentItem);
+
+        // Seasons
+        {
+            QTreeWidgetItem *childItem = new QTreeWidgetItem(parentItem);
+            childItem->setForeground(0, brush);
+            childItem->setText(0, QString("Seasons: %1").arg(show.seasons));
+        }
+        // Status
+        {
+            QTreeWidgetItem *childItem = new QTreeWidgetItem(parentItem);
+            childItem->setForeground(0, brush);
+            childItem->setText(0, QString("Status: %1").arg(show.status));
+        }
+        // Started
+        {
+            QTreeWidgetItem *childItem = new QTreeWidgetItem(parentItem);
+            childItem->setForeground(0, brush);
+            childItem->setText(0, QString("Started: %1").arg(show.started));
+        }
+        // Ended
+        if (show.ended != 0) {
+            QTreeWidgetItem *childItem = new QTreeWidgetItem(parentItem);
+            childItem->setForeground(0, brush);
+            childItem->setText(0, QString("Ended: %1").arg(show.ended));
+        }
+        // Classification
+        {
+            QTreeWidgetItem *childItem = new QTreeWidgetItem(parentItem);
+            childItem->setForeground(0, brush);
+            childItem->setText(0, QString("Classification: %1").arg(show.classification));
+        }
+        // Genres
+        if (!show.genres.empty()) {
+            QTreeWidgetItem *childItem = new QTreeWidgetItem(parentItem);
+            childItem->setForeground(0, brush);
+            childItem->setText(0, QString("Genres: %1").arg(show.genres.join(", ")));
+        }
+
+        displayedShowCounter++;
+    } // foreach()
+
+    if (m_searchResults.count() != 0) {
+        ui.cbFilterResults->setEnabled(true);
+        ui.lblDisplayed->setEnabled(true);
+        if (displayedShowCounter != 0) {
+            ui.treeSearchResults->setEnabled(true);
+            ui.treeSearchResults->setRootIsDecorated(true);
+        } else {
+            ui.treeSearchResults->setEnabled(false);
+            QTreeWidgetItem *item = new QTreeWidgetItem();
+            item->setText(0, tr("Only ended shows were found!"));
+            ui.treeSearchResults->addTopLevelItem(item);
+            ui.treeSearchResults->setRootIsDecorated(false);
+        }
+    } else {
+        ui.treeSearchResults->setEnabled(false);
+        ui.cbFilterResults->setEnabled(false);
+        ui.lblDisplayed->setEnabled(false);
+
+        QTreeWidgetItem *item = new QTreeWidgetItem();
+        item->setText(0, tr("No results found!"));
+        ui.treeSearchResults->addTopLevelItem(item);
+        ui.treeSearchResults->setRootIsDecorated(false);
     }
+
+    ui.lblDisplayed->setText(QString("Displayed: %1/%2")
+                             .arg(displayedShowCounter)
+                             .arg(m_searchResults.count()));
 } // displaySearchResults()
 
 
