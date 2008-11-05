182cd82fe74350fffb08bffaf5f7400527cf8f38
gcnweb <gcnweb@e6d63c4d-7831-0410-afa5-55a26f18ab34>
2008-11-05 16:13:21 +0000

    Almost working (though there are some remaining bugs to squash)

--- playground/ScrollTextTest/view.h
+++ playground/ScrollTextTest/view.h
@@ -1,26 +1,59 @@
+/*
+** Copyright (C) 2008 Gilles CHAUVIN <gcnweb+nextshows@gmail.com>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2  of  the  License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be  useful, but
+** WITHOUT  ANY  WARRANTY;  without  even  the   implied   warranty  of
+** MERCHANTABILITY or FITNESS FOR A  PARTICULAR  PURPOSE. See  the  GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU  General  Public  License
+** along with  this  program;  if  not,  write  to  the  Free  Software
+** Foundation, Inc.,
+** 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #ifndef __VIEW_H__
 #define __VIEW_H__
 
+
 #include <QtGui>
+
 
 class View : public QGraphicsView
 {
-    Q_OBJECT;
+    Q_OBJECT
 
 public:
     View(QWidget *parent = 0);
     ~View();
 
+    void addCreditSection(const QString &text,
+                          const int &fontSize = 12);
+    void addCredit(const QString &country,
+                   const QByteArray &name,
+                   const QString &link,
+                   const QString &task);
+    void startScrolling();
+
 protected:
-    void timerEvent(QTimerEvent *event);
     void resizeEvent(QResizeEvent *event);
 
+private Q_SLOTS:
+    void moveText();
+
 private:
-    QWidget *m_scrollWidget;
-    QGraphicsProxyWidget *m_scrollWidgetProxy;
-    QGridLayout *m_gridLayout;
-    QGraphicsScene *m_scene;
-    int m_timer;
+    QTimer               *m_timer;
+    QGraphicsScene       *m_scene;
+    QGridLayout          *m_gridLayout;
+    QGraphicsProxyWidget *m_scrollWidget;
 };
 
 #endif // __VIEW_H__
+
+
+// EOF - vim:ts=4:sw=4:et:
