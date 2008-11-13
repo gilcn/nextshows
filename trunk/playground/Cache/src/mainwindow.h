737c5cc97c025b7d690bab8685d815de16797781
kh.starlifter <kh.starlifter@e6d63c4d-7831-0410-afa5-55a26f18ab34>
2008-11-13 18:24:33 +0000

    work on cache class

--- trunk/playground/Cache/src/mainwindow.h
+++ trunk/playground/Cache/src/mainwindow.h
@@ -1,5 +1,5 @@
 /*
-** ** Copyright (C) 2008 Emmanuel HAMELET <kh.starlifter@gmail.com>
+** Copyright (C) 2008 Emmanuel HAMELET <kh.starlifter@gmail.com>
 ** Copyright (C) 2008 Gilles CHAUVIN <gcnweb+nextshows@gmail.com>
 **
 ** This program is free software; you can redistribute it and/or modify
