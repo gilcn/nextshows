3ccbe0b2adcecc9f40fa720a69ef5a8c76973837
gcnweb <gcnweb@e6d63c4d-7831-0410-afa5-55a26f18ab34>
2008-11-18 22:22:28 +0000

    Import dbinterface from playground (that class is not yet completed though)

---
+++ trunk/nextShows/src/libs/dbinterface.h
@@ -0,0 +1,65 @@
+/*
+** Copyright (C) 2008 Emmanuel Hamelet <kh.starlifter@gmail.com>
+** Copyright (C) 2008 Gilles Chauvin <gcnweb+nextshows@gmail.com>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2  of  the  License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be  useful, but
+** WITHOUT  ANY  WARRANTY;  without  even  the   implied   warranty  of
+** MERCHANTABILITY or FITNESS FOR A  PARTICULAR  PURPOSE. See  the  GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU  General  Public  License
+** along with  this  program;  if  not,  write  to  the  Free  Software
+** Foundation, Inc.,
+** 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef __DBINTERFACE_H__
+#define __DBINTERFACE_H__
+
+
+// Own
+#include "nextshows.h"
+// QtSql
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+
+
+class DbInterface
+{
+public:
+    DbInterface();
+    ~DbInterface();
+
+    // Init the database
+    bool init();
+
+    void saveUserShows(const QList<NextShows::ShowInfos_t> &shows);
+    QList<NextShows::ShowInfos_t> readUserShows();
+    QList<uint> expiredShow(const int &timestamp);
+
+//signals:
+//    void stateChanged(const QString &text);
+
+private:
+    QSqlDatabase m_db;
+    QSqlQuery m_query;
+    enum RecordType {
+            Insert,
+            Update
+        };
+
+    bool createTables();
+    bool saveShow(const NextShows::ShowInfos_t &show, const RecordType &rtype);
+    bool deleteShow(const uint &id);
+};
+
+
+#endif // __DBINTERFACE_H__
+
+
+// EOF - vim:ts=4:sw=4:et:
